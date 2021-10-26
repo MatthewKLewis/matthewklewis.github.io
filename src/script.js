eeab587bf048fe48b174748bfbcf8c1280ed2051
MatthewKLewis <matthew.knight.lewis@gmail.com>
2021-10-26 15:02:24 -0400

    fog

--- src/script.js
+++ src/script.js
@@ -15,12 +15,19 @@ function getScrollPercent() {
         sh = 'scrollHeight';
     return (h[st]||b[st]) / ((h[sh]||b[sh]) - h.clientHeight) * 100;
 }
+function getMidpointPixels() {
+    var h = document.documentElement,
+        b = document.body,
+        st = 'scrollTop',
+        sh = 'scrollHeight';
+    return ((h[sh]||b[sh]) / 3);
+}
 document.addEventListener('scroll', (evt)=>{
     scrollPercent = getScrollPercent().toFixed(2)
     STATS_ELEMENT.innerText = 'Animation Offset: ' + scrollPercent
 })
 HOME_ELEMENT.addEventListener('click', ()=>{console.log(HOME_ELEMENT);  window.scrollTo({top: 0, behavior: 'smooth'})})
-SKILLS_ELEMENT.addEventListener('click', ()=>{console.log(SKILLS_ELEMENT);  window.scrollTo({top: 930, behavior: 'smooth'})})
+SKILLS_ELEMENT.addEventListener('click', ()=>{console.log(SKILLS_ELEMENT);  window.scrollTo({top: getMidpointPixels(), behavior: 'smooth'})})
 CONTACTS_ELEMENT.addEventListener('click', ()=>{console.log(CONTACTS_ELEMENT);  window.scrollTo({top: 9999999, behavior: 'smooth'})})
 
 //#region [rgba(25, 128, 128, 0.15) ] RENDERER (VIEW)
@@ -72,16 +79,15 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 camera.lookAt(0,0,0)
 
 const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshLambertMaterial( { color: 0x00ff00 } );
+const material = new THREE.MeshLambertMaterial( { color: 0xffffff } );
 const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
 
 const light = new THREE.SpotLight(0xFFFFFF, 1);
 light.position.x = 1.3
 light.position.z = 1
 scene.add(light);
-
-scene.add( cube );
-console.log(scene);
+scene.fog = new THREE.FogExp2( '#1a2b31', 0.9 );console.log(scene);
 //#endregion
 
 //#region [rgba(255, 255, 255, 0.15) ] UPDATE
